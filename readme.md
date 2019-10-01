0036feedf7588bd4e5e3599b4d346300dc8c4109
Brian Rogers <rogers.brian@gmail.com>
2019-10-01 09:03:54 -0700

    Initial commit of Next.js, React.js, and Jest.js framework

---
+++ readme.md
@@ -0,0 +1,27 @@
+# Read Me Information
+
+## Setup
+
+npm install --save next react react-dom typescript
+
+npm install --save-dev babel-jest @babel/preset-env @babel/preset-react react-test-renderer
+
+### package.json
+
+```json
+{
+  "scripts": {
+    "dev": "next",
+    "build": "next build",
+    "start": "next start"
+  }
+}
+```
+
+### VS Code settings.json
+
+```json
+{
+    "jest.showCoverageOnLoad": true
+}
+```
---
+++ tests/math.test.js
@@ -0,0 +1,25 @@
+'using strict'
+
+const m = require('../utils/math')
+
+it('Math Prototypes: ceil10: should return 7 when the ceil10 of 0 for 19 divided by 3', () => {
+    expect(Math.ceil10(19 / 3, 0)).toBe(7)
+})
+
+it('Math Prototypes: floor10: should return 6 when getting the floor 10 of 0 for 19 divided by 3', () => {
+    expect(Math.round10(19 / 3, 0)).toBe(6)
+})
+
+// Negative number test
+it('Math Prototypes: floor10: should return -6 when getting the floor 10 of 0 for -19 divided by -3', () => {
+    expect(Math.round10(-19 / 3, -1)).toBe(-6.3)
+})
+
+it('Math Prototypes: round10: should return 6.3 when rounding to the -1 for 19 divided by 3', () => {
+    expect(Math.round10(19 / 3, -1)).toBe(6.3)
+})
+
+// Null value
+it('Math Prototypes: round10: should return NaN if a null is passed', () => {
+    expect(Math.round10(NaN, -1)).toBe(NaN)
+})
