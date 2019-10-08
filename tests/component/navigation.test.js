bb6eae91853af9c3ddd542b14e5abc5f6386d287
Brian Rogers <rogers.brian@gmail.com>
2019-10-08 07:40:57 -0700

    Added selectable test for navigation

--- tests/component/navigation.test.js
+++ tests/component/navigation.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { shallow, mount, render } from 'enzyme';
+import { shallow } from 'enzyme';
 
 import Navigation from '../../component/navigation'
 
@@ -7,4 +7,8 @@ describe('component:navigation - rendering', () => {
     it('should render without throwing and error', () => {
         expect(shallow(<Navigation />).contains(<div id="nav_links"></div>))
     })
+
+    it('should be selectable by ID "nav_links"', function() {
+        expect(shallow(<Navigation />).is('#nav_links')).toBe(true);
+    });
 })
