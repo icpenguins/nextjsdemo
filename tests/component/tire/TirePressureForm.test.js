6e8bc6d7d7ac40c955698d2be11a948628e5f5b7
Brian Rogers <rogers.brian@gmail.com>
2019-10-09 08:54:37 -0700

    Added test case for TirePressureForm

---
+++ tests/component/tire/TirePressureForm.test.js
@@ -0,0 +1,68 @@
+'using strict'
+
+//import Enzyme from 'enzyme'
+//import Adapter from 'enzyme-adapter-react-16'
+import { shallow } from 'enzyme'
+//import fetch from 'isomorphic-unfetch'
+import TirePressureForm from '../../../component/tire/TirePressureForm'
+
+//Enzyme.configure({ adapter: new Adapter() });
+
+//jest.mock('isomorphic-unfetch')
+
+describe('component:tire:pressuredform - tire pressure form', () => {
+
+    it('should be div id=tirePressureForm', () => {
+        let wrapper = shallow(<TirePressureForm />)
+
+        expect(wrapper.find('#tirePressureForm').length).toBe(1)
+    })
+
+    it('should be set the state value for max_load', () => {
+        let wrapper = shallow(<TirePressureForm />)
+        let propName = 'max_load'
+        let propValue = 3860
+
+        let load = wrapper.find(`input[name="${propName}"]`)
+        load.simulate('change', { target: { name: propName, value: propValue } })
+
+        expect(wrapper.state(propName)).toBe(propValue)
+    })
+
+    it('should be set the state value for max_psi', () => {
+        let wrapper = shallow(<TirePressureForm />)
+        let propName = 'max_psi'
+        let propValue = 65
+
+        let load = wrapper.find(`input[name="${propName}"]`)
+        load.simulate('change', { target: { name: propName, value: propValue } })
+
+        expect(wrapper.state(propName)).toBe(propValue)
+    })
+
+    it('must click and must accept enter', () => {
+        let wrapper = shallow(<TirePressureForm />)
+        const props = [
+            {
+                name: 'max_load',
+                value: 3860
+            },
+            {
+                name: 'max_psi',
+                value: 65
+            }
+        ]
+
+        const event = {
+            keyCode: 13,
+            preventDefault: jest.fn()
+        }
+
+        wrapper.find(`input[name="${props[0].name}"]`).simulate('change', { target: { name: props[0].name, value: props[0].value } })
+        wrapper.find(`input[name="${props[1].name}"]`).simulate('change', { target: { name: props[1].name, value: props[1].value } })
+        wrapper.find('button').simulate('click', event)
+        wrapper.find('button').simulate('keyDown', event)
+
+        expect(event.preventDefault.mock.calls.length).toBe(2)
+    })
+})
